08805bcea04a312495d091bd90ae4a4eb8cf5087
Nahidujjaman Hridoy <hridoyboss12@gmail.com>
2022-06-01 23:15:08 +0600

    forum comment

--- src/pages/Forum/Forum.js
+++ src/pages/Forum/Forum.js
@@ -1,38 +1,53 @@
 import React from "react";
+import { useForm } from "react-hook-form";
 import { useQuery } from "react-query";
 import { useParams } from "react-router-dom";
 import interceptor from "../../utils/interceptor";
+import myAxios from "../../utils/myAxios";
 
 const Forum = () => {
   const { id } = useParams();
-  const { data, isLoading } = useQuery('singleForum', () => {
-    return interceptor.get(`/api/forum_post/${id}`)
-  })
-  console.log(data);
-  if (isLoading) {
-    return <div className="flex w-screen h-screen justify-center items-center">
-      <svg role="status" className="w-24 h-24 mr-2 text-gray-200 animate-spin dark:text-gray-600 fill-blue-600" viewBox="0 0 100 101" fill="none" xmlns="http://www.w3.org/2000/svg">
-        <path d="M100 50.5908C100 78.2051 77.6142 100.591 50 100.591C22.3858 100.591 0 78.2051 0 50.5908C0 22.9766 22.3858 0.59082 50 0.59082C77.6142 0.59082 100 22.9766 100 50.5908ZM9.08144 50.5908C9.08144 73.1895 27.4013 91.5094 50 91.5094C72.5987 91.5094 90.9186 73.1895 90.9186 50.5908C90.9186 27.9921 72.5987 9.67226 50 9.67226C27.4013 9.67226 9.08144 27.9921 9.08144 50.5908Z" fill="currentColor"></path>
-        <path d="M93.9676 39.0409C96.393 38.4038 97.8624 35.9116 97.0079 33.5539C95.2932 28.8227 92.871 24.3692 89.8167 20.348C85.8452 15.1192 80.8826 10.7238 75.2124 7.41289C69.5422 4.10194 63.2754 1.94025 56.7698 1.05124C51.7666 0.367541 46.6976 0.446843 41.7345 1.27873C39.2613 1.69328 37.813 4.19778 38.4501 6.62326C39.0873 9.04874 41.5694 10.4717 44.0505 10.1071C47.8511 9.54855 51.7191 9.52689 55.5402 10.0491C60.8642 10.7766 65.9928 12.5457 70.6331 15.2552C75.2735 17.9648 79.3347 21.5619 82.5849 25.841C84.9175 28.9121 86.7997 32.2913 88.1811 35.8758C89.083 38.2158 91.5421 39.6781 93.9676 39.0409Z" fill="currentFill"></path>
-      </svg>
-    </div>
-  }
+  const {
+    register,
+    handleSubmit,
+    watch,
+    formState: { errors },
+  } = useForm();
+  const { data, isLoading, refetch } = useQuery("singleForum", () => {
+    return interceptor.get(`/api/forum_post/${id}`);
+  });
 
+  const onSubmit = (data) => {
+    console.log(data);
+    myAxios.post(`api/forum_comment/${id}/`, data).then((res) => {
+      refetch();
+    });
+  };
+  if (isLoading) {
+    return (
+      <div className="flex w-screen h-screen justify-center items-center">
+        <svg
+          role="status"
+          className="w-24 h-24 mr-2 text-gray-200 animate-spin dark:text-gray-600 fill-blue-600"
+          viewBox="0 0 100 101"
+          fill="none"
+          xmlns="http://www.w3.org/2000/svg"
+        >
+          <path
+            d="M100 50.5908C100 78.2051 77.6142 100.591 50 100.591C22.3858 100.591 0 78.2051 0 50.5908C0 22.9766 22.3858 0.59082 50 0.59082C77.6142 0.59082 100 22.9766 100 50.5908ZM9.08144 50.5908C9.08144 73.1895 27.4013 91.5094 50 91.5094C72.5987 91.5094 90.9186 73.1895 90.9186 50.5908C90.9186 27.9921 72.5987 9.67226 50 9.67226C27.4013 9.67226 9.08144 27.9921 9.08144 50.5908Z"
+            fill="currentColor"
+          ></path>
+          <path
+            d="M93.9676 39.0409C96.393 38.4038 97.8624 35.9116 97.0079 33.5539C95.2932 28.8227 92.871 24.3692 89.8167 20.348C85.8452 15.1192 80.8826 10.7238 75.2124 7.41289C69.5422 4.10194 63.2754 1.94025 56.7698 1.05124C51.7666 0.367541 46.6976 0.446843 41.7345 1.27873C39.2613 1.69328 37.813 4.19778 38.4501 6.62326C39.0873 9.04874 41.5694 10.4717 44.0505 10.1071C47.8511 9.54855 51.7191 9.52689 55.5402 10.0491C60.8642 10.7766 65.9928 12.5457 70.6331 15.2552C75.2735 17.9648 79.3347 21.5619 82.5849 25.841C84.9175 28.9121 86.7997 32.2913 88.1811 35.8758C89.083 38.2158 91.5421 39.6781 93.9676 39.0409Z"
+            fill="currentFill"
+          ></path>
+        </svg>
+      </div>
+    );
+  }
 
   return (
     <div className="py-4">
-      {/* <div className="relative w-full h-96">
-        <img
-          src={data.data.blogimg}
-          alt={data.data.blogtitle}
-          className="w-full h-full object-cover brightness-50"
-        />
-        <div className="text-white absolute inset-0 h-full w-full flex justify-center items-center">
-          <h2 className="text-center text-5xl italic font-semibold">
-            {data.data.blogtitle}
-          </h2>
-        </div>
-      </div> */}
       <p
         className="
     first-letter:text-5xl
@@ -46,18 +61,70 @@ const Forum = () => {
       >
         {data.data.content}
       </p>
-      {
-        data?.data?.categories?.map((item, index) => {
-          return (
-            <div className="flex justify-center items-center my-4">
-              <div className="bg-blue-500 text-white text-2xl rounded-full px-4 py-1">
-                {item.name}
+      {/* Comments Lists */}
+      {data?.data?.comments?.map((comment, index) => {
+        return (
+          <div key={index} className="my-4">
+            <div className="flex items-center">
+              <img
+                src={comment.user.profile_pic}
+                alt={comment.user.username}
+                className="w-10 h-10 rounded-full mr-4"
+              />
+              <div className="flex-1">
+                <p className="text-sm font-semibold">{comment.user.username}</p>
+                <p className="text-sm">{comment.comment}</p>
               </div>
             </div>
-          )
-        }
-        )
-      }
+          </div>
+        );
+      })}
+      <div className="flex mx-auto items-center justify-center shadow-lg mx-8 mb-4">
+        <form
+          onSubmit={handleSubmit(onSubmit)}
+          className="w-full bg-white rounded-lg px-4 pt-2"
+        >
+          <div className="flex flex-wrap -mx-3 mb-6">
+            <h2 className="px-4 pt-3 pb-2 text-gray-800 text-lg">
+              Add a new comment
+            </h2>
+            <div className="w-full md:w-full px-3 mb-2 mt-2">
+              <textarea
+                className="bg-gray-100 rounded border border-gray-400 leading-normal resize-none w-full h-20 py-2 px-3 font-medium placeholder-gray-700 focus:outline-none focus:bg-white"
+                name="body"
+                placeholder="Type Your Comment"
+                required
+                {...register("comment", { required: true })}
+              ></textarea>
+            </div>
+            <div className="w-full md:w-full flex items-start md:w-full px-3">
+              <div className="flex items-start w-1/2 text-gray-700 px-2 mr-auto">
+                <svg
+                  fill="none"
+                  className="w-5 h-5 text-gray-600 mr-1"
+                  viewBox="0 0 24 24"
+                  stroke="currentColor"
+                >
+                  <path
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                    stroke-width="2"
+                    d="M13 16h-1v-4h-1m1-4h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z"
+                  />
+                </svg>
+                <p className="text-xs md:text-sm pt-px">Some HTML is okay.</p>
+              </div>
+              <div className="-mr-1">
+                <input
+                  type="submit"
+                  className="bg-white text-gray-700 font-medium py-1 px-4 border border-gray-400 rounded-lg tracking-wide mr-1 hover:bg-gray-100"
+                  value="Post Comment"
+                />
+              </div>
+            </div>
+          </div>
+        </form>
+      </div>
     </div>
   );
 };
