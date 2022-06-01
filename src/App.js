0c381b1ad6021bc8b535513f97135965053f5b0d
Nahidujjaman Hridoy <hridoyboss12@gmail.com>
2022-06-01 21:58:53 +0600

    Profile and Edit

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import { Route, Routes } from "react-router-dom";
 import { ToastContainer } from "react-toastify";
-import 'react-toastify/dist/ReactToastify.css';
+import "react-toastify/dist/ReactToastify.css";
 import "./App.css";
 import Footer from "./components/Footer/Footer";
 import Header from "./components/Header/Header";
@@ -10,6 +10,7 @@ import Blogs from "./pages/Blogs/Blogs";
 import Cart from "./pages/Cart/Cart";
 import Complain from "./pages/Complain/Complain";
 import Complains from "./pages/Complains/Complains";
+import EditProfile from "./pages/EditProfile/EditProfile";
 import Forum from "./pages/Forum/Forum";
 import Forums from "./pages/Forums/Forums";
 import Home from "./pages/Home/Home";
@@ -30,22 +31,86 @@ function App() {
 
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/new-blog" element={<RequireAuth><NewBlog /></RequireAuth>} />
+        <Route
+          path="/new-blog"
+          element={
+            <RequireAuth>
+              <NewBlog />
+            </RequireAuth>
+          }
+        />
         <Route path="/blogs" element={<Blogs />} />
         <Route path="/blogs/:id" element={<Blog />} />
-        <Route path="/new-forum" element={<RequireAuth><NewForum /></RequireAuth>} />
+        <Route
+          path="/new-forum"
+          element={
+            <RequireAuth>
+              <NewForum />
+            </RequireAuth>
+          }
+        />
         <Route path="/forums" element={<Forums />} />
         <Route path="/forums/:id" element={<Forum />} />
-        <Route path="/new-complain" element={<RequireAuth><NewComplain /></RequireAuth>} />
-        <Route path="/new-product" element={<RequireAuth><NewProduct /></RequireAuth>} />
-        <Route path="/complains" element={<RequireAuth><Complains /></RequireAuth>} />
-        <Route path="/complain/:id" element={<RequireAuth><Complain /></RequireAuth>} />
+        <Route
+          path="/new-complain"
+          element={
+            <RequireAuth>
+              <NewComplain />
+            </RequireAuth>
+          }
+        />
+        <Route
+          path="/new-product"
+          element={
+            <RequireAuth>
+              <NewProduct />
+            </RequireAuth>
+          }
+        />
+        <Route
+          path="/complains"
+          element={
+            <RequireAuth>
+              <Complains />
+            </RequireAuth>
+          }
+        />
+        <Route
+          path="/complain/:id"
+          element={
+            <RequireAuth>
+              <Complain />
+            </RequireAuth>
+          }
+        />
         <Route path="/products" element={<Products />} />
         <Route path="/product/:id" element={<Product />} />
         <Route path="/signin" element={<SignIn />} />
         <Route path="/signup" element={<SignUp />} />
-        <Route path="/profile" element={<RequireAuth><Profile /></RequireAuth>} />
-        <Route path="/cart" element={<RequireAuth><Cart /></RequireAuth>} />
+        <Route
+          path="/profile"
+          element={
+            <RequireAuth>
+              <Profile />
+            </RequireAuth>
+          }
+        />
+        <Route
+          path="/editprofile/:id"
+          element={
+            <RequireAuth>
+              <EditProfile />
+            </RequireAuth>
+          }
+        />
+        <Route
+          path="/cart"
+          element={
+            <RequireAuth>
+              <Cart />
+            </RequireAuth>
+          }
+        />
       </Routes>
       <Footer />
       <ToastContainer />
---
+++ src/pages/EditProfile/EditProfile.js
@@ -0,0 +1,118 @@
+import React from "react";
+import { useForm } from "react-hook-form";
+import { useQuery } from "react-query";
+import { useNavigate, useParams } from "react-router-dom";
+import myAxios from "../../utils/myAxios";
+
+const EditProfile = () => {
+  const {
+    register,
+    handleSubmit,
+    watch,
+    formState: { errors },
+    setValue,
+  } = useForm();
+  let navigate = useNavigate();
+  let { id } = useParams();
+  const { data, isLoading, refetch } = useQuery("user", () =>
+    myAxios(`user/${id}`)
+  );
+  if (isLoading) return <p>Loading...</p>;
+  const onSubmit = (submitData) => {
+    const payLoad = {
+      full_name: submitData.full_name,
+      student_id: submitData.student_id,
+      profile_pic: submitData.profile_pic[0],
+    };
+
+    myAxios
+      .patch(`user/${id}/`, payLoad, {
+        headers: {
+          "Content-Type": "multipart/form-data",
+        },
+      })
+      .then((res) => {
+        refetch();
+        navigate("/profile");
+      });
+  };
+  setValue("full_name", data?.data?.full_name);
+  setValue("student_id", data?.data?.student_id);
+  return (
+    <div>
+      <h1>Edit Profile</h1>
+      <form
+        className="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4"
+        onSubmit={handleSubmit(onSubmit)}
+      >
+        <div className="mb-4">
+          <label
+            className="block text-gray-700 text-sm font-bold mb-2"
+            htmlFor="name"
+          >
+            Name
+          </label>
+          <input
+            className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+            id="name"
+            type="text"
+            required
+            placeholder="name"
+            {...register("full_name", { required: true })}
+          />
+          {errors.full_name && (
+            <p className="text-red-500 text-xs italic">Field is required</p>
+          )}
+        </div>
+        <div className="mb-4">
+          <label
+            className="block text-gray-700 text-sm font-bold mb-2"
+            htmlFor="name"
+          >
+            Student ID
+          </label>
+          <input
+            className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+            id="name"
+            type="text"
+            required
+            placeholder="12345678"
+            {...register("student_id", { required: true })}
+          />
+          {errors.student_id && (
+            <p className="text-red-500 text-xs italic">Field is required</p>
+          )}
+        </div>
+        <div className="mb-4">
+          <label
+            className="block text-gray-700 text-sm font-bold mb-2"
+            htmlFor="name"
+          >
+            Profile Picture
+          </label>
+          <input
+            className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+            id="name"
+            type="file"
+            required
+            {...register("profile_pic", { required: true })}
+          />
+          {errors.profile_pic && (
+            <p className="text-red-500 text-xs italic">Field is required</p>
+          )}
+        </div>
+
+        <div className="flex items-center justify-between">
+          <button
+            className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
+            type="submit"
+          >
+            Edit
+          </button>
+        </div>
+      </form>
+    </div>
+  );
+};
+
+export default EditProfile;
